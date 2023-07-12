efaa582d839f5c5bf225aeac29d51064fd8afaff
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-07-12 17:09:30 -0500

    Formatting

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ import asyncio
 from kasa import Discover, SmartPlug
 from yaspin import yaspin
 
+
 def signal_handler(sig, frame):
-    print('\nTerminating program.')
+    print("\nTerminating program.")
     sys.exit(0)
+
 
 def check_battery(lower_lim, upper_lim):
     battery = psutil.sensors_battery()
     level = int(battery.percent)
 
-    if (level >= upper_lim):
+    if level >= upper_lim:
         return "X"
-    elif (level <= lower_lim):
+    elif level <= lower_lim:
         return "O"
     else:
         return "X"
-    
+
+
 async def find_plug(alias):
     devices = await Discover.discover()
 
@@ -30,8 +33,9 @@ async def find_plug(alias):
             return addr
         else:
             continue
-    
+
     return False
+
 
 async def main(plug_addr):
     plug = SmartPlug(plug_addr)
@@ -43,16 +47,22 @@ async def main(plug_addr):
             await plug.turn_off()
         elif status == "O":
             await plug.turn_on()
-    
+
         time.sleep(10)
 
 
 signal.signal(signal.SIGINT, signal_handler)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--plug", type=str, required=True, help="plug you want to modify")
-parser.add_argument("-l", "--lower-limit", type=int, default=30, help="battery lower limit")
-parser.add_argument("-u", "--upper-limit", type=int, default=80, help="battery upper limit")
+parser.add_argument(
+    "-p", "--plug", type=str, required=True, help="plug you want to modify"
+)
+parser.add_argument(
+    "-l", "--lower-limit", type=int, default=30, help="battery lower limit"
+)
+parser.add_argument(
+    "-u", "--upper-limit", type=int, default=80, help="battery upper limit"
+)
 
 args = parser.parse_args()
 
